35d130aa789f4d16c4349b7ba458bfad5aed3899
imrochamatheus <im.rochamatheus@gmail.com>
2021-04-07 10:59:57 -0300

    Acrescentando layout

--- functions.php
+++ functions.php
@@ -1,31 +1,41 @@
 <?php
+
+    //Função para busca de pokemon
     function pokemon_query($i) {
     
+        //Concatenando url com id do pokemon
         $url = 'https://pokeapi.co/api/v2/pokemon/'.$i;
 
+        //Obtendo dados da página e convertendo Json
         $pokemon = file_get_contents($url);
         $pokemon = json_decode($pokemon);
 
+        $name =  $pokemon->forms[0]->name;
+        $hp = $pokemon->stats[0]->base_stat;
+        $attack = $pokemon->stats[1]->base_stat;
+        $defense = $pokemon->stats[2]->base_stat;
+        $special_attack = $pokemon->stats[3]->base_stat;
+        $special_defense = $pokemon->stats[4]->base_stat;
+        $speed = $pokemon->stats[5]->base_stat;
+        $total = $hp + $attack +  $defense + $special_attack + $special_defense + $speed;
+    
+        //Preenchendo array de características
         $caracteristicas = 
         [
-            'name' => $pokemon->forms[0]->name,
-            'hp' => $pokemon->stats[0]->base_stat,
-            'attack' => $pokemon->stats[1]->base_stat,
-            'defense' => $pokemon->stats[2]->base_stat,
-            'special_attack' => $pokemon->stats[3]->base_stat,
-            'special_defense' => $pokemon->stats[4]->base_stat,
-            'speed' => $pokemon->stats[5]->base_stat,
-            'total' => $pokemon->stats[0]->base_stat +
-                        $pokemon->stats[1]->base_stat +
-                        $pokemon->stats[2]->base_stat +
-                        $pokemon->stats[3]->base_stat +
-                        $pokemon->stats[4]->base_stat +
-                        $pokemon->stats[5]->base_stat
+            'name' => $name,
+            'hp' => $hp,
+            'attack' => $attack,
+            'defense' => $defense,
+            'special_attack' => $special_attack,
+            'special_defense' => $special_defense,
+            'speed' => $speed,
+            'total' => $total
         ];
 
         return $caracteristicas;
     }   
 
+    //Função de ordenação
     function order(){
         $pokemons = []; 
 
--- index.php
+++ index.php
@@ -9,57 +9,96 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <title>PokeList</title>
 </head>
 <body>
-    <header>
-        <div class="container">
-            <h1>PokeTable</h1>
-        </div>
-    </header>
-    <div class="container">
-        <table>
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>HP</th>
-                    <th>Atk</th>
-                    <th>Def</th>
-                    <th>Sp.Atk</th>
-                    <th>Sp.Def</th>
-                    <th>Speed</th>
-                    <th>Total</th>
-                </tr>
-            </thead>
+  <div class="container-fluid">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-3">
+                  <img src="assets/img/pokemon-logo.png" alt="Logo" id="logo">
+              </div>                
+          </div>
+          <!-- Borda externa da tela-->
+          <div id="screen-border">
 
-            <?php  
-           
-                $caracteristicas = order();
-                for($i = 0; $i < 60; $i++){
+              <!-- visor -->
+              <div id="screen" class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Hp</th>
+                            <th>Attack</th>
+                            <th>Defense</th>
+                            <th>Sp.Attack</th>
+                            <th>Sp.Defense</th>
+                            <th>Speed</th>
+                            <th>Total</th>
+                            </tr>
+                        </thead>
 
-                    $detalhes = $caracteristicas[$i];
-            ?>
-            <tbody>
-                <tr>
-                    <td><?php echo $detalhes['name'] ;?></td>
-                    <td><?php echo $detalhes['hp'] ;?></td>
-                    <td><?php echo $detalhes['attack'] ;?></td>
-                    <td><?php echo $detalhes['defense'] ;?></td>
-                    <td><?php echo $detalhes['special_attack'] ;?></td>
-                    <td><?php echo $detalhes['special_defense'] ;?></td>
-                    <td><?php echo $detalhes['speed'] ;?></td>
-                    <td><?php echo $detalhes['total'] ;?></td>
-                </tr>
-            </tbody>
-            <?php
+                        <?php  
+                            $caracteristicas = order();
+                            for($i = 0; $i < 60; $i++){
 
-                }
-            ?>
-    </table>
+                            $detalhes = $caracteristicas[$i];
+                        ?>
+
+                        <tbody>
+                            <tr>
+                                <td><img src="assets/img/ahshssda.png" alt="icon" id="icone"></td>
+                                <td><?php echo $detalhes['name'] ;?></td>
+                                <td><?php echo $detalhes['hp'] ;?></td>
+                                <td><?php echo $detalhes['attack'] ;?></td>
+                                <td><?php echo $detalhes['defense'] ;?></td>
+                                <td><?php echo $detalhes['special_attack'] ;?></td>
+                                <td><?php echo $detalhes['special_defense'] ;?></td>
+                                <td><?php echo $detalhes['speed'] ;?></td>
+                                <td><?php echo $detalhes['total'] ;?></td>
+                            </tr>
+                        </tbody>
+
+                        <?php
+                            }
+                        ?>
+                    </table>   
+              </div>
+          </div> 
+          <div class="row">
+              <div class="col-md-2">
+                  <span id="btn"></span>
+              </div>
+              <div class="col-md-3">
+                  <span id="btn-2"></span>
+                  <span id="btn-3"></span>
+              </div>
+              <div class="col-md-3">
+                  
+              </div>
+              <div class="col-md-4">
+                  <span><img src="assets/img/pikachu-removebg-preview.png" alt="Pikachu" id="poke"></span>
+              </div>
+          </div>
+      </div>
   </div>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script src="loader/js"></script>
 </body>
 </html>
 
